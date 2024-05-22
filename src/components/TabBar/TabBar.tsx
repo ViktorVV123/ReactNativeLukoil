7e0a46bc3ec25388f14ac5ebf5fb0f6611f2e817
vlasyukviv <Viktor.Vlasyuk@lukoil.com>
2024-05-22 17:29:23 +0300

    добавил карусель

--- src/components/TabBar/TabBar.tsx
+++ src/components/TabBar/TabBar.tsx
@@ -12,6 +12,7 @@ import {EstatesNavigate} from '../Navigate/Estates/EstatesNavigate.tsx';
 import {CloudIcon} from '../../icons/HeaderIcon/CloudIcon.tsx';
 import {ChartIcon} from '../../icons/HeaderIcon/ChartIcon.tsx';
 import {MenuIcon} from '../../icons/HeaderIcon/MenuIcon.tsx';
+import {Carusel} from '../Navigate/Carusel/Carusel.tsx';
 
 const defaultHeaderStyles = {
   headerStyle: {backgroundColor: '#000000'},
@@ -81,6 +82,16 @@ export const TabBar = () => {
       <Tab.Screen
         name="Estates"
         component={EstatesNavigate}
+        options={{
+          tabBarIcon: ({color}: {color: string}) => (
+            <EstatesIcon fill={color} />
+          ),
+          ...defaultHeaderStyles,
+        }}
+      />
+      <Tab.Screen
+        name="Carusel"
+        component={Carusel}
         options={{
           tabBarIcon: ({color}: {color: string}) => (
             <EstatesIcon fill={color} />
